f9ec342fa3c99f79ac855c47643cda7087de22e1
romanocry <jathiere.romain@gmail.com>
2025-01-23 00:19:28 +0100

    Update const.py
    disable the network part to add a manualy value

--- custom_components/tarif-edf/const.py
+++ custom_components/tarif-edf/const.py
@@ -1,36 +1,13 @@
-"""Constants for the Tarif EDF integration."""
-
+"""Constants for the Electricity Tarif integration."""
 from homeassistant.const import Platform
 
 DOMAIN = "tarif_edf"
+CONTRACT_TYPE_BASE = "base"
+CONTRACT_TYPE_HPHC = "hphc"
 
-CONTRACT_TYPE_BASE="base"
-CONTRACT_TYPE_HPHC="hphc"
-CONTRACT_TYPE_TEMPO="tempo"
-
-TARIF_BASE_URL="https://www.data.gouv.fr/fr/datasets/r/c13d05e5-9e55-4d03-bf7e-042a2ade7e49"
-TARIF_HPHC_URL="https://www.data.gouv.fr/fr/datasets/r/f7303b3a-93c7-4242-813d-84919034c416"
-
-TEMPO_COLOR_API_URL="https://www.api-couleur-tempo.fr/api/jourTempo/today"
-
-TEMPO_PRICES_DETAILS={
-    1: {
-        "couleur": "bleu",
-        "hc": 0.1296,
-        "hp": 0.1609
-    },
-    2: {
-        "couleur": "blanc",
-        "hc": 0.1486,
-        "hp": 0.1894
-    },
-    3: {
-        "couleur": "rouge",
-        "hc": 0.1568,
-        "hp": 0.7562
-    }
-}
-
-DEFAULT_REFRESH_INTERVAL=1
+# Valeurs par défaut pour les tarifs
+DEFAULT_HC_PRICE = 0.1470  # Prix par défaut en heures creuses
+DEFAULT_HP_PRICE = 0.1841  # Prix par défaut en heures pleines
+DEFAULT_BASE_PRICE = 0.1740  # Prix par défaut pour le tarif base
 
 PLATFORMS = [Platform.SENSOR]
